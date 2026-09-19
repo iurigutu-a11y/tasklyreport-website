c7770362d0762eaf09e8bc913a29dd29454bd0aa
iurigutu-a11y <iurigutu@gmail.com>
2026-09-19 23:07:50 +0200

    Add CleanFAKT privacy and support pages

---
+++ cleanfakt/config.js
@@ -0,0 +1,5 @@
+window.CLEANFAKT_CONFIG = Object.freeze({
+  supportEmail: 'iurigutu@gmail.com',
+  privacyUrl: 'https://tasklyreport.com/cleanfakt/privacy/',
+  supportUrl: 'https://tasklyreport.com/cleanfakt/support/'
+});
